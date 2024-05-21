5aad3c2a4e66100e8bbf78c481392a735c7f4298
Jeesjose <150226742+Jeesjose@users.noreply.github.com>
2024-05-21 14:47:29 +0530

    Add files via upload

---
+++ FSD 22/script.js
@@ -0,0 +1,58 @@
+// postfix increment
+// let a= 1;
+//  let b= a++;
+//  console.log(a)
+//  console.log(b)
+
+// prefix increment
+// let c= 2;
+//  let d= c++;
+//  console.log(c)
+//  console.lo
+// var toothbrush=new Object()
+//     toothbrush.brand="close up";
+//     toothbrush.color="pink";
+
+//  console.log(toothbrush)no
+// concation
+// let fname="leo";
+// let iname="messi";
+//  console.log(`$(fname)$(iname)`)
+//  let m=5;
+//  let n=6;
+// console.log(`the sum is $(m+n)`)
+// array
+// let fruits =["apple","mango","oranger,23"]
+// console.log(fruits);
+// fruits.push("grapes");
+// console.log(fruits);
+// console.log(fruits.length);
+// fruits.2="pineapple";
+// console.log(fruits);
+// fruits.unshift("fruits");
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+
+
+// array of object
+// let bsc=[
+    
+//     {fname:"messi",
+//     Age:38,
+//  },
+//  {fname:"neymar",
+//     Age:32,
+//  },
+//  {fname:"cristano",
+//     Age:40,
+//  },
+// ];
+// console.log(bsc[0].Age);
+function fullname(fname,iname){
+    console.log(`$(frame)$(iname)`);
+}
+
+
